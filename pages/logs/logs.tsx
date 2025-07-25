90ba9a6f757657848eb28662f8660bee2aa0ce3c
Duraisamy303 <psmkduraisamy@gmail.com>
2025-07-25 11:28:22 +0530

    logs file added

--- pages/logs/logs.tsx
+++ pages/logs/logs.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Table } from 'antd';
 import Models from '@/imports/models.import';
 import moment from 'moment';
 import CommonLoader from '@/components/commonLoader';
 import { useSetState } from '@/utils/function.util';
 import Pagination from '@/components/pagination/pagination';
+import { Form, Input, Table, Spin } from 'antd';
+import useDebounce from '@/components/useDebounce/useDebounce';
 
 const Logs = () => {
+    const { Search } = Input;
+
     const [state, setState] = useSetState({
         logList: [],
         currentPage: 1,
@@ -15,16 +18,30 @@ const Logs = () => {
     useEffect(() => {
         getData(state.currentPage);
     }, []);
+    const debouncedSearch = useDebounce(state.search);
+
+    useEffect(() => {
+        getData(state.currentPage);
+    }, [debouncedSearch]);
 
     const getData = async (page: number) => {
         try {
             setState({ loading: true });
-            const res = await Models.logs.logList(page);
+            const body = bodyData();
+            const res = await Models.logs.logList(page, body);
             tableFormat(res, page);
         } catch (error) {
             setState({ loading: false });
             console.log('error: ', error);
         }
+    };
+
+    const bodyData = () => {
+        const body: any = {};
+        if (state.search) {
+            body['search'] = state.search;
+        }
+        return body;
     };
 
     const tableFormat = (res: any, page: number) => {
@@ -88,9 +105,17 @@ const Logs = () => {
     return (
         <>
             <div className="panel">
-                <div className="tax-heading-main">
+                <div className="tax-heading-main justify-between items-center mb-4 flex">
                     <div>
                         <h1 className="text-lg font-semibold dark:text-white-light">Logs</h1>
+                    </div>
+                    <div>
+                        <Search placeholder="Search IP Address" value={state.search} onChange={(e) => setState({ search: e.target.value })} enterButton className="search-bar" />
+                        <Search placeholder="Search IP Address" value={state.search} onChange={(e) => setState({ search: e.target.value })} enterButton className="search-bar" />
+                   
+                    </div>
+
+                    <div>
                     </div>
                 </div>
 
